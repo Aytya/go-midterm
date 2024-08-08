a855b02a13e9f8dc1c6db4716d4d4372be0752bb
“Your <98690555+Aytya@users.noreply.github.com>
2024-08-08 17:36:40 +0500

    updated

--- main.go
+++ main.go
@@ -50,7 +50,7 @@ func main() {
 
 	r := chi.NewRouter()
 	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"wails://wails.localhost:34115"},
+		AllowedOrigins:   []string{"wails://wails.localhost:34115", "http://localhost:5174"},
 		AllowCredentials: true,
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
 		AllowedHeaders:   []string{"Content-Type"},
